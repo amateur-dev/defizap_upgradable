13bbbf4319590ff97b637d0a7499e5cd4380f762
amateur-dev <me@dipeshsukhani.dev>
2020-04-21 17:14:43 +0800

    Balancer General and MKR deployed

--- networks.js
+++ networks.js
@@ -20,7 +20,7 @@ module.exports = {
     mainnet: {
       provider: () => new HDWalletProvider(process.env.PrivateKey, "https://mainnet.infura.io/v3/" + infuraProjectId),
       networkId: 1,       // mainnet's id
-      gasPrice: 10000000000,
+      gasPrice: 5000000000,
     },
     rinkeby: {
       provider: () => new HDWalletProvider(process.env.PrivateKey, "https://rinkeby.infura.io/v3/" + infuraProjectId),
