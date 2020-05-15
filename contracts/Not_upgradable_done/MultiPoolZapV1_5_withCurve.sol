a76ee3b41ca874d8ed7001938981e73ce2848ee3
amateur-dev <me@dipeshsukhani.dev>
2020-05-16 07:30:35 +0800

    fix to multipool v1_5

--- contracts/Not_upgradable_done/MultiPoolZapV1_5_withCurve.sol
+++ contracts/Not_upgradable_done/MultiPoolZapV1_5_withCurve.sol
@@ -1,4 +1,4 @@
-// Copyright (C) 2019, 2020 dipeshsukhani, nodar, suhailg, apoorvlathey
+// Copyright (C) 2019, 2020 dipeshsukhani, nodar, suhailg, apoorvlathey, seb, sumit
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -18,54 +18,83 @@ import "../../node_modules/@openzeppelin/contracts-ethereum-package/contracts/to
 
 pragma solidity ^0.5.0;
 
+
 interface UniswapFactoryInterface {
-    function getExchange(address token) external view returns (address exchange);
+    function getExchange(address token)
+        external
+        view
+        returns (address exchange);
 }
+
 
 interface UniswapExchangeInterface {
-    function tokenToEthTransferInput(uint256 tokens_sold, uint256 min_eth, uint256 deadline, address recipient) external returns (uint256  eth_bought);
-    function getTokenToEthInputPrice(uint256 tokens_sold) external view returns (uint256 eth_bought);
+    function tokenToEthTransferInput(
+        uint256 tokens_sold,
+        uint256 min_eth,
+        uint256 deadline,
+        address recipient
+    ) external returns (uint256 eth_bought);
+
+    function getTokenToEthInputPrice(uint256 tokens_sold)
+        external
+        view
+        returns (uint256 eth_bought);
 }
 
+
 interface uniswapPoolZap {
-    function LetsInvest(address _TokenContractAddress, address _towhomtoissue) external payable returns (uint256);
+    function LetsInvest(address _TokenContractAddress, address _towhomtoissue)
+        external
+        payable
+        returns (uint256);
 }
+
+
 interface ICurvePoolZapIn {
     function ZapIn(
         address _toWhomToIssue,
         address _IncomingTokenAddress,
         address _curvePoolExchangeAddress,
         uint256 _IncomingTokenQty
-    ) external payable returns(uint256 crvTokensBought);
+    ) external payable returns (uint256 crvTokensBought);
 }
+
 
 /**
     @title Multiple Pool Zap
     @author Zapper
-    @notice Use this contract to Add liquidity to Multiple Uniswap Pools at once using ETH or ERC20
+    @notice Use this contract to Add liquidity to Multiple Pools at once using ETH or ERC20
 */
 contract MultiPoolZapV1_5 is Ownable {
-    using SafeMath for uint;
+    using SafeMath for uint256;
 
     uniswapPoolZap public uniswapPoolZapAddress;
     UniswapFactoryInterface public UniswapFactory;
     ICurvePoolZapIn public CurvePoolZapIn;
-    
+
     uint16 public goodwill;
     address payable public dzgoodwillAddress;
     mapping(address => uint256) private userBalance;
 
-    constructor(uint16 _goodwill, address payable _dzgoodwillAddress, address _curvePoolZapInAddress) public {
+    constructor(
+        uint16 _goodwill,
+        address payable _dzgoodwillAddress,
+        address _curvePoolZapInAddress
+    ) public {
         goodwill = _goodwill;
         dzgoodwillAddress = _dzgoodwillAddress;
-        uniswapPoolZapAddress = uniswapPoolZap(0x97402249515994Cc0D22092D3375033Ad0ea438A);
-        UniswapFactory = UniswapFactoryInterface(0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95);
+        uniswapPoolZapAddress = uniswapPoolZap(
+            0x97402249515994Cc0D22092D3375033Ad0ea438A
+        );
+        UniswapFactory = UniswapFactoryInterface(
+            0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95
+        );
         CurvePoolZapIn = ICurvePoolZapIn(_curvePoolZapInAddress);
     }
 
     function set_new_goodwill(uint16 _new_goodwill) public onlyOwner {
         require(
-            _new_goodwill > 0 && _new_goodwill < 10000,
+            _new_goodwill >= 0 && _new_goodwill < 10000,
             "GoodWill Value not allowed"
         );
         goodwill = _new_goodwill;
@@ -78,18 +107,24 @@ contract MultiPoolZapV1_5 is Ownable {
         dzgoodwillAddress = _new_dzgoodwillAddress;
     }
 
-    function set_uniswapPoolZapAddress(address _uniswapPoolZapAddress) onlyOwner public {
+    function set_uniswapPoolZapAddress(address _uniswapPoolZapAddress)
+        public
+        onlyOwner
+    {
         uniswapPoolZapAddress = uniswapPoolZap(_uniswapPoolZapAddress);
     }
 
-    function set_UniswapFactory(address _UniswapFactory) onlyOwner public {
+    function set_UniswapFactory(address _UniswapFactory) public onlyOwner {
         UniswapFactory = UniswapFactoryInterface(_UniswapFactory);
     }
-    
-    function set_curvePoolZapInAddress(address _curvePoolZapIn) onlyOwner public {
+
+    function set_curvePoolZapInAddress(address _curvePoolZapIn)
+        public
+        onlyOwner
+    {
         CurvePoolZapIn = ICurvePoolZapIn(_curvePoolZapIn);
     }
-    
+
     /**
         @notice Add liquidity to Multiple Uniswap Pools at once using ETH or ERC20
         @param _IncomingTokenContractAddress The token address for ERC20 being deposited. Input address(0) in case of ETH deposit.
@@ -98,29 +133,40 @@ contract MultiPoolZapV1_5 is Ownable {
         @param _curvePoolAddresses Array of CurvePool Exchange Addresses to Zap
         @param respectiveWeightedValues Array of Relative Ratios (corresponding to underlyingTokenAddresses) to proportionally distribute received ETH or ERC20 into various pools.
     */
-    function multipleZapIn (
+    function multipleZapIn(
         address _IncomingTokenContractAddress,
         uint256 _IncomingTokenQty,
         address[] memory underlyingTokenAddresses,
         address[] memory _curvePoolAddresses,
-        uint256[] memory respectiveWeightedValues
+        uint16[] memory respectiveWeightedValues
     ) public payable {
-
-        uint totalWeights;
-        require(underlyingTokenAddresses.length + _curvePoolAddresses.length == respectiveWeightedValues.length, "Input array lengths incorrect");
-        for(uint i=0; i<respectiveWeightedValues.length; i++) {
+        uint256 totalWeights;
+        require(
+            underlyingTokenAddresses.length + _curvePoolAddresses.length ==
+                respectiveWeightedValues.length,
+            "Input array lengths incorrect"
+        );
+        for (uint256 i = 0; i < respectiveWeightedValues.length; i++) {
             totalWeights = (totalWeights).add(respectiveWeightedValues[i]);
         }
 
         uint256 eth2Trade;
 
         if (msg.value > 0) {
-            require (_IncomingTokenContractAddress == address(0), "Incoming token address should be address(0)");
+            require(
+                _IncomingTokenContractAddress == address(0),
+                "Incoming token address should be address(0)"
+            );
             eth2Trade = msg.value;
-        } else if(_IncomingTokenContractAddress == address(0) && msg.value == 0) {
+        } else if (
+            _IncomingTokenContractAddress == address(0) && msg.value == 0
+        ) {
             revert("Please send ETH along with function call");
-        } else if(_IncomingTokenContractAddress != address(0)) {
-            require(msg.value == 0, "Cannot send Tokens and ETH at the same time");
+        } else if (_IncomingTokenContractAddress != address(0)) {
+            require(
+                msg.value == 0,
+                "Cannot send Tokens and ETH at the same time"
+            );
             require(
                 IERC20(_IncomingTokenContractAddress).transferFrom(
                     msg.sender,
@@ -130,67 +176,66 @@ contract MultiPoolZapV1_5 is Ownable {
                 "Error in transferring ERC20"
             );
             eth2Trade = _token2Eth(
-              _IncomingTokenContractAddress,
-              _IncomingTokenQty,
-              address(this)
+                _IncomingTokenContractAddress,
+                _IncomingTokenQty
             );
         }
 
-        uint goodwillPortion = ((eth2Trade).mul(goodwill)).div(10000);
-        uint totalInvestable = (eth2Trade).sub(goodwillPortion);
-        uint totalLeftToBeInvested = totalInvestable;
+        uint256 goodwillPortion = ((eth2Trade).mul(goodwill)).div(10000);
+        uint256 totalInvestable = (eth2Trade).sub(goodwillPortion);
+        uint256 totalLeftToBeInvested = totalInvestable;
 
         require(address(dzgoodwillAddress).send(goodwillPortion));
 
-        uint residualETH;
-        for (uint i=0; i<underlyingTokenAddresses.length; i++) {
-            uint LPT = uniswapPoolZapAddress.LetsInvest.value((((totalInvestable).mul(respectiveWeightedValues[i])).div(totalWeights)+residualETH))(underlyingTokenAddresses[i], address(this));
-            IERC20(UniswapFactory.getExchange(address(underlyingTokenAddresses[i]))).transfer(msg.sender, LPT);
-            totalLeftToBeInvested = (totalLeftToBeInvested).sub(((totalInvestable).mul(respectiveWeightedValues[i])).div(totalWeights));
+        uint256 residualETH;
+        // ZapIn Uniswap Pools
+        for (uint256 i = 0; i < underlyingTokenAddresses.length; i++) {
+            uint256 LPT = uniswapPoolZapAddress.LetsInvest.value(
+                (((totalInvestable).mul(respectiveWeightedValues[i])).div(
+                    totalWeights
+                ) + residualETH)
+            )(underlyingTokenAddresses[i], address(this));
+            IERC20(
+                UniswapFactory.getExchange(address(underlyingTokenAddresses[i]))
+            )
+                .transfer(msg.sender, LPT);
+            totalLeftToBeInvested = (totalLeftToBeInvested).sub(
+                ((totalInvestable).mul(respectiveWeightedValues[i])).div(
+                    totalWeights
+                )
+            );
             residualETH = (address(this).balance).sub(totalLeftToBeInvested);
         }
-        
+
         //ZapIn to Curve Pools
-        uint uniswapPoolsCount = underlyingTokenAddresses.length;
-        for(uint i=0; i<_curvePoolAddresses.length; i++) {
+        uint256 uniswapPoolsCount = underlyingTokenAddresses.length;
+        for (uint256 i = 0; i < _curvePoolAddresses.length; i++) {
             CurvePoolZapIn.ZapIn.value(
-                (((totalInvestable).mul(respectiveWeightedValues[i + uniswapPoolsCount])).div(totalWeights)+residualETH)
+                ((
+                    (totalInvestable).mul(
+                        respectiveWeightedValues[i + uniswapPoolsCount]
+                    )
+                )
+                    .div(totalWeights) + residualETH)
             )(msg.sender, address(0), _curvePoolAddresses[i], 0);
-            residualETH=0;
-        }
-
-        if(address(this).balance >= 250000000000000000){
-            totalInvestable = address(this).balance;
-            totalLeftToBeInvested = totalInvestable;
             residualETH = 0;
-            for (uint i=0;i<underlyingTokenAddresses.length;i++) {
-                uint LPT = uniswapPoolZapAddress.LetsInvest.value((((totalInvestable).mul(respectiveWeightedValues[i])).div(totalWeights)+residualETH))(underlyingTokenAddresses[i], address(this));
-                IERC20(UniswapFactory.getExchange(address(underlyingTokenAddresses[i]))).transfer(msg.sender, LPT);
-                totalLeftToBeInvested = (totalLeftToBeInvested).sub(((totalInvestable).mul(respectiveWeightedValues[i])).div(totalWeights));
-                residualETH = (address(this).balance).sub(totalLeftToBeInvested);
-            }
         }
-
-        userBalance[msg.sender] = address(this).balance;
-        require (send_out_eth(msg.sender));
     }
 
     /**
-        @notice This function swaps ERC20 to ERC20 via Uniswap
+        @notice This function swaps ERC20 to ETH via Uniswap
         @param _FromTokenContractAddress Address of Token to swap
-        @param tokens2Trade The quantity of tokens to swap
-        @param _toWhomToIssue Address of user to send the swapped ETH to
+        @param tokens2Trade The quantity of tokens to swaps
         @return The amount of ETH Received.
     */
-    function _token2Eth(
-        address _FromTokenContractAddress,
-        uint256 tokens2Trade,
-        address _toWhomToIssue
-    ) internal returns (uint256 ethBought) {
+    function _token2Eth(address _FromTokenContractAddress, uint256 tokens2Trade)
+        internal
+        returns (uint256 ethBought)
+    {
 
-            UniswapExchangeInterface FromUniSwapExchangeContractAddress
-         = UniswapExchangeInterface(
-            UniswapFactory.getExchange(_FromTokenContractAddress)
+        UniswapExchangeInterface FromUniSwapExchangeContractAddress
+        = UniswapExchangeInterface(
+        UniswapFactory.getExchange(_FromTokenContractAddress)
         );
 
         IERC20(_FromTokenContractAddress).approve(
@@ -200,27 +245,21 @@ contract MultiPoolZapV1_5 is Ownable {
 
         ethBought = FromUniSwapExchangeContractAddress.tokenToEthTransferInput(
             tokens2Trade,
-            ((FromUniSwapExchangeContractAddress.getTokenToEthInputPrice(tokens2Trade)).mul(99).div(100)),
+            (
+                (
+                    FromUniSwapExchangeContractAddress.getTokenToEthInputPrice(
+                        tokens2Trade
+                    )
+                )
+                    .mul(99)
+                    .div(100)
+            ),
             SafeMath.add(block.timestamp, 300),
-            _toWhomToIssue
+            address(this)
         );
         require(ethBought > 0, "Error in swapping Eth: 1");
     }
 
-    /**
-        @notice This function sends the user's remaining ETH back to them.
-        @param _towhomtosendtheETH The Address of the user
-        @return Boolean corresponding to successful execution.
-    */
-    function send_out_eth(address _towhomtosendtheETH) internal returns (bool) {
-        require(userBalance[_towhomtosendtheETH] > 0);
-        uint256 amount = userBalance[_towhomtosendtheETH];
-        userBalance[_towhomtosendtheETH] = 0;
-        (bool success, ) = _towhomtosendtheETH.call.value(amount)("");
-        return success;
-    }
-
     // - fallback function let you / anyone send ETH to this wallet without the need to call any function
-    function() external payable {
-    }
+    function() external payable {}
 }
